4e633d8483ca06f3831b465483f5da200107e33c
kokoro514 <hatanokokoroyo@gmail.com>
2023-05-18 03:00:55 +0800

    临时优化

--- retrun_test/return_test.py
+++ retrun_test/return_test.py
@@ -13,49 +13,50 @@ from stragegy.strategy import FundOperation
 from index.bollinger_bands_indicator import calculate_bollinger_bands
 from index.move_average_line_indicator import calculate_move_average_line
 
-show_chart_flag = False
-bollinger_bands_time_window = 30
-bollinger_bands_standard_deviation = 2.5
+show_chart_flag = True
+bollinger_bands_time_window = 20
+bollinger_bands_standard_deviation = 2
 
 end_date = '2023-05-15'
-years = 3
+years = 1
 # end_date - years
 start_date = pd.to_datetime(end_date) - pd.DateOffset(years=years)
+start_date = start_date.strftime('%Y-%m-%d')
 
 
 def main():
     # strategy = DoubleMoveAverageLineStrategyImpl()
     strategy = BollingerBandsStrategyImpl()
-    return_test(strategy, start_date, end_date, years)
+    return_test(strategy)
 
 
-def return_test(strategy, start_date, end_date, years):
+def return_test(strategy):
     """
     计算收益率
-    :param years:
     :param strategy: 策略
-    :param start_date: 开始日期
-    :param end_date: 结束日期
     :return:  data_frame include columns: MA<time_window>
     """
     fund_code_list = get_fund_code_list_for_test()
     return_rate_is_zero_fund_code_list = []
     return_rate_less_than_5_fund_code_list = []
     return_rate_greater_than_5_fund_code_list = []
-    total_return_rate = 0
+    total_years_avg_return_rate = 0
     for fund_code in fund_code_list:
-        # 三年年化利率
-        return_rate, fund_name = calculate_return_rate(fund_code, strategy, start_date, end_date)
-        if return_rate == 0:
-            return_rate_is_zero_fund_code_list.append({fund_code, return_rate, fund_name})
+        # 指定时间段内的年化利率
+        years_avg_return_rate, fund_name = calculate_years_avg_return_rate(fund_code, strategy)
+        if years_avg_return_rate == 0:
+            return_rate_is_zero_fund_code_list.append({fund_code, years_avg_return_rate, fund_name})
             continue
-        if return_rate < 0.05:
-            return_rate_less_than_5_fund_code_list.append([fund_code, return_rate, fund_name])
+        if years_avg_return_rate < 0.05:
+            return_rate_less_than_5_fund_code_list.append([fund_code, years_avg_return_rate, fund_name])
         else:
-            return_rate_greater_than_5_fund_code_list.append([fund_code, return_rate, fund_name])
-        total_return_rate += return_rate
-    print('三年平均年化利率: ' + str(
-        total_return_rate / (len(fund_code_list) - len(return_rate_is_zero_fund_code_list)) / years))
+            return_rate_greater_than_5_fund_code_list.append([fund_code, years_avg_return_rate, fund_name])
+        total_years_avg_return_rate += years_avg_return_rate
+    valid_fund_count = (len(fund_code_list) - len(return_rate_is_zero_fund_code_list))
+    if valid_fund_count == 0:
+        print('没有有效的基金')
+        return
+    print('三年平均年化利率: ' + str(total_years_avg_return_rate / valid_fund_count))
     print('以下基金收益率为0: ' + str(return_rate_is_zero_fund_code_list))
     print('以下基金年化收益率小于5%: ' + str(return_rate_less_than_5_fund_code_list))
     print('以下基金年化收益率大于5%: ' + str(return_rate_greater_than_5_fund_code_list))
@@ -63,24 +64,23 @@ def return_test(strategy, start_date, end_date, years):
 
 def get_fund_code_list_for_test():
     fund_code_list = [
-        '016814', '004598', '012863', '001156', '400015', '012832', '017482', '006229', '013311', '001856', '010149',
-        '011103', '008702', '006221', '005506', '016814', '004598', '007882', '008591', '015675', '004855', '012863',
-        '014111', '012832', '400015', '001156', '005064', '010149', '006229', '011036', '006221', '010210', '012549',
-        '012414', '013311', '008089', '001676', '005506', '001676', '011103', '011840', '011463', '009180', '007818',
-        '008702', '013081', '013219', '008591', '005918', '016814', '012810', '004598', '007882', '008087', '012769',
-        '008586', '005224', '017090', '014130', '012637', '012615', '017482', '001924', '002258', '001678', '013621',
-        '010769', '017484', '006166', '001856', '008888', '320007',
+        # '016814', '004598', '012863', '001156', '400015', '012832', '017482', '006229', '013311', '001856', '010149',
+        # '011103', '008702', '006221', '005506', '016814', '004598', '007882', '008591', '015675', '004855', '012863',
+        # '014111', '012832', '400015', '001156', '005064', '010149', '006229', '011036', '006221', '010210', '012549',
+        # '012414', '013311', '008089', '001676', '005506', '001676', '011103', '011840', '011463', '009180', '007818',
+        # '008702', '013081', '013219', '008591', '005918', '016814', '012810', '004598', '007882', '008087', '012769',
+        # '008586', '005224', '017090', '014130', '012637', '012615', '017482', '001924', '002258', '001678', '013621',
+        # '010769', '017484', '006166', '001856', '008888', '320007',
+        '012615'
     ]
     return set(fund_code_list)
 
 
-def calculate_return_rate(fund_code, strategy, start_date, end_date):
+def calculate_years_avg_return_rate(fund_code, strategy):
     """
     计算单个基金的收益率
     :param fund_code: 基金代码
     :param strategy: 策略类
-    :param start_date: 开始时间
-    :param end_date: 结束时间
     :return:
     """
     print('============================================================')
@@ -97,7 +97,7 @@ def calculate_return_rate(fund_code, strategy, start_date, end_date):
     # 当前买入时间
     last_buy_date = None
     # 查询基金在指定时间内的历史净值
-    fund_name, net_worth_data_list = get_fund_detail2(fund_code, start_date, end_date)
+    fund_name, net_worth_data_list = get_fund_detail2(fund_code)
     # 从第三十天开始计算
     for i in range(30, len(net_worth_data_list)):
         current_net_worth_data_list = net_worth_data_list[0:i]
@@ -139,15 +139,16 @@ def calculate_return_rate(fund_code, strategy, start_date, end_date):
         return_rate = round(total_profit / total_cost, 4)
     else:
         return_rate = 0
+    years_avg_return_rate = round(return_rate / years, 4)
     print(fund_code, 'current period cost:', total_cost, 'total profit:', round(total_profit, 4), ', return rate:',
-          return_rate, fund_name)
+          years_avg_return_rate, fund_name)
     # net_worth_data_list生成折线图
-    show_chart(net_worth_data_list, fund_code, fund_name, return_rate)
+    show_chart(net_worth_data_list, fund_code, fund_name, years_avg_return_rate)
 
-    return return_rate, fund_name
+    return years_avg_return_rate, fund_name
 
 
-def get_fund_detail2(fund_code: str, start_date, end_date):
+def get_fund_detail2(fund_code: str):
     fund_detail = get_fund_detail(fund_code, start_date, end_date)
     net_worth_data_list = fund_detail.get_net_worth_data_list()
     # if last day is not today, add today's net worth
@@ -195,7 +196,7 @@ def show_chart(net_worth_data_list: list, fund_code, fund_name, return_rate):
     data_frame = calculate_move_average_line(data_frame, 20)
 
     plt.plot(data_frame['date'], data_frame['upper_band'], color='red', label='upper band')
-    plt.plot(data_frame['date'], data_frame['middle_band'], color='pink', label='middle band')
+    plt.plot(data_frame['date'], data_frame['middle_band'], color='orange', label='middle band')
     plt.plot(data_frame['date'], data_frame['lower_band'], color='green', label='lower band')
     plt.fill_between(data_frame.index, data_frame['lower_band'], data_frame['upper_band'], alpha=0.2)  # 填充布林带之间的区域
 
